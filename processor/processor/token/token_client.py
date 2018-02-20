c96562b0cc6d87b692c4515c194eca235d44b84e
Yevhenii Babichenko <eugene.babichenko@gmail.com>
2018-02-20 15:49:29 +0200

    fix package registration and starting

--- processor/processor/token/token_client.py
+++ processor/processor/token/token_client.py
@@ -14,7 +14,7 @@
 # ------------------------------------------------------------------------
 from processor.protos.token_pb2 import TokenPayload, Transfer
 from processor.shared.basic_client import BasicClient
-from processor.rem_token.token_handler import TokenHandler
+from processor.token.token_handler import TokenHandler
 from protobuf3_to_dict import protobuf_to_dict
 
 # TODO in progress
