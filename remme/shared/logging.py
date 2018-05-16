30a9a1d1a7b73d03ad53e777a66552f5fd488a11
Yevhenii Babichenko <eugene.babichenko@gmail.com>
2018-05-16 13:47:41 +0300

    Add copyright notices to ws

--- remme/shared/logging.py
+++ remme/shared/logging.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ------------------------------------------------------------------------
+
 import logging
 from pathlib import Path
 from sawtooth_sdk.processor.log import init_console_logging
