d6633c7dbb9213b0c8c950ad70ea7725b90b75f0
Andriy Bogomazov <andrii.bogomazov@gmail.com>
2018-07-17 11:33:13 +0300

    fix bug

--- remme/rest_api/block_info.py
+++ remme/rest_api/block_info.py
@@ -4,9 +4,10 @@ from remme.shared.utils import from_proto_to_json
 
 block_info_client = BlockInfoClient()
 
+
 def get_block_config():
     return from_proto_to_json(block_info_client.get_block_info_config())
 
 
-def get_block_info(block_start, block_end):
-    return [from_proto_to_json(block) for block in block_info_client.get_many_block_info(block_start, block_end)]
+def get_block_info(start, end):
+    return [from_proto_to_json(block) for block in block_info_client.get_many_block_info(start, end)]
