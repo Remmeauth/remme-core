7b3a5b947e68d1652a996e00c1f2fa635e33e4c6
Andriy Bogomazov <andrii.bogomazov@gmail.com>
2018-07-24 14:25:20 +0300

    remove extra fields

--- remme/tp/basic.py
+++ remme/tp/basic.py
@@ -131,9 +131,7 @@ class BasicHandler(TransactionHandler):
         event_name = state_processor[transaction_payload.method].get(EMIT_EVENT, None)
         if event_name:
             add_event(context, event_name,
-                      {"write_addresses": [key for key, _ in updated_state.items()],
-                       "family_name": self.family_name,
-                       "payload_method": transaction_payload.method})
+                      {"write_addresses": [key for key, _ in updated_state.items()]})
 
     def make_address(self, appendix):
         address = self._prefix + appendix
