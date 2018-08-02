a2f71801c4709a9e80fe4601aa51c61cc362b86e
Yevhenii Babichenko <eugene.babichenko@gmail.com>
2018-08-02 15:30:25 +0300

    Start refactoring docker-compose

---
+++ bash/poet-validator-registry.sh
@@ -0,0 +1,3 @@
+if [ \"$$REMME_CONSENSUS\" = \"poet-simulator\" ]; then
+    poet-validator-registry-tp -vv -C tcp://validator:4004
+fi
---
+++ bash/validator.sh
@@ -0,0 +1,68 @@
+ADDITIONAL_ARGS=""
+
+if [ ! -f /etc/sawtooth/keys/validator.priv ]; then
+    sawadm keygen
+fi
+
+if [ "$REMME_START_MODE" = "genesis" ]; then
+    if [ -d /var/lib/sawtooth ]; then
+        rm /var/lib/sawtooth/*
+    fi
+
+    sawset genesis -k /etc/sawtooth/keys/validator.priv
+    GENESIS_BATCHES="config-genesis.batch"
+
+    if [ "$REMME_CONSENSUS" = "poet-simulator" ]; then
+        sawset proposal create \
+            -k /etc/sawtooth/keys/validator.priv \
+            sawtooth.consensus.algorithm=poet \
+            "sawtooth.poet.report_public_key_pem=$(cat /etc/sawtooth/simulator_rk_pub.pem)" \
+            "sawtooth.poet.valid_enclave_measurements=$(poet enclave --enclave-module simulator measurement)" \
+            "sawtooth.poet.valid_enclave_basenames=$(poet enclave --enclave-module simulator basename)" \
+            sawtooth.poet.enclave_module_name=sawtooth_poet_simulator.poet_enclave_simulator.poet_enclave_simulator \
+            -o poet_config.batch
+
+        poet registration create \
+            -k /etc/sawtooth/keys/validator.priv \
+            --enclave-module simulator \
+            -o poet_genesis.batch
+
+        GENESIS_BATCHES="$GENESIS_BATCHES poet_config.batch poet_genesis.batch"
+    fi
+
+    sawset proposal create \
+        -k /etc/sawtooth/keys/validator.priv \
+        sawtooth.validator.batch_injectors=block_info \
+        "sawtooth.validator.block_validation_rules=NofX:1,block_info;XatY:block_info,0;local:0" \
+        -o block_config.batch
+
+    sawset proposal create \
+        -k /etc/sawtooth/keys/validator.priv \
+        "remme.settings.pub_key_encryption=$(cat /etc/sawtooth/keys/validator.pub)" \
+        "remme.settings.genesis_owners=$(cat /etc/sawtooth/keys/validator.pub)" \
+        remme.settings.swap_comission=100 \
+        -o settings_config.batch
+
+    GENESIS_BATCHES="$GENESIS_BATCHES block_config.batch settings_config.batch"
+
+    sawadm genesis $GENESIS_BATCHES
+fi
+
+if [ "$REMME_START_MODE" = "run" ]; then
+
+    ADDITIONAL_ARGS="--peering $REMME_PEERING_MODE"
+    if [ ! -z "$REMME_PEERS_LIST" ]; then
+        ADDITIONAL_ARGS="$ADDITIONAL_ARGS --peers $REMME_PEERS_LIST"
+    fi
+    if [ ! -z "$REMME_SEEDS_LIST" ]; then
+        ADDITIONAL_ARGS="$ADDITIONAL_ARGS --seeds $REMME_SEEDS_LIST"
+    fi
+fi
+
+sawtooth-validator -vv \
+    --endpoint tcp://$REMME_VALIDATOR_IP:$REMME_VALIDATOR_PORT \
+    --bind component:tcp://127.0.0.1:4004 \
+    --bind network:tcp://0.0.0.0:8800 \
+    --scheduler parallel \
+    --minimum-peer-connectivity $REMME_MINIMUM_PEERS_CONNECTIVITY \
+    $ADDITIONAL_ARGS
