7b71e4e22015e6eef986e77b03f082fe13a1c325
Bogdan Kurinnyi <bogdan.kurinniy@ambisafe.co>
2018-08-30 11:56:03 +0300

    REM-471: Wrape Stream as singletone

--- remme/shared/utils.py
+++ remme/shared/utils.py
@@ -44,3 +44,10 @@ def message_to_dict(message):
         message,
         including_default_value_fields=True,
         preserving_proto_field_name=True)
+
+class Singleton(type):
+    _instances = {}
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
