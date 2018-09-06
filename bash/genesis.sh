b331b07034d37bf53ae0705363d43f45174a5341
Yevhenii Babichenko <eugene.babichenko@gmail.com>
2018-09-06 13:08:15 +0300

    Remove user key generation command
    As now we use node's key to sign everything we don't need to
    generate user's keys anymore. Moreover, Sawtooth CLI tools are not
    installed into this our Docker containers.

--- bash/genesis.sh
+++ bash/genesis.sh
@@ -1,7 +1,4 @@
 if [ "$REMME_START_MODE" = "genesis" ]; then
-    if [ ! -f /etc/sawtooth/keys/validator.priv ]; then
-        sawtooth keygen key
-    fi
     if [ ! -e /genesis/batch ]; then
         mkdir /genesis/batch
     fi
