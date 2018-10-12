f00ae32a3dd94c31a5043f9c09d58fbd1fdd93fd
Yevhenii Babichenko <eugene.babichenko@gmail.com>
2018-10-12 13:17:36 +0300

    Fix remmetechnical preparaion script

--- build/ci/prepare-compose.sh
+++ build/ci/prepare-compose.sh
@@ -3,4 +3,4 @@
 REMMETECHNICAL_REGEX='s/remme\/([a-z-]+):latest/remmetechnical\/\1:latest/g'
 
 sed -i -E -e $REMMETECHNICAL_REGEX ./build/docker-compose.yml
-sed -i -E -e $REMMETECHNICAL_REGEX ./compose/*.yml
+sed -i -E -e $REMMETECHNICAL_REGEX ./docker/compose/*.yml
