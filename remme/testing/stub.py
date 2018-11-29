50a81848c24dfe313f061d5917f0cce85f3117ed
Dmytro Striletskyi <dmytro.striletskyi@gmail.com>
2018-11-29 11:53:30 +0200

    REM-844: create add events and set state implementation

--- remme/testing/stub.py
+++ remme/testing/stub.py
@@ -1,5 +1,5 @@
 """
-StubContext is ..
+Provide implementation of the stub context, the transaction context sandbox.
 
 StubContext particularly repeat Sawtooth context, additional knowledge could be got from its documentation.
 
@@ -19,14 +19,33 @@ class StubContextEvent:
     """
 
     def __init__(self, event_type, attributes, data=None):
+        """
+        Initialize object.
+
+        Arguments:
+            event_type (str): a type of the event.
+            attributes (list of (str, str) tuples or None): an event attributes.
+            data (bytes or None): Additional information about the event.
+        """
         self._event_type = event_type
         self._attributes = attributes
         self._data = data
 
 
 class StubContext:
+    """
+    Stub context, the transaction context sandbox.
+    """
 
     def __init__(self, inputs, outputs, initial_state):
+        """
+        Initialize object.
+
+        Arguments:
+            inputs (list): a list of inputs.
+            outputs (list): a list of outputs.
+            initial_state (dict): initial state of the context.
+        """
         self.inputs = inputs
         self.outputs = outputs
         self._state = initial_state
@@ -39,9 +58,9 @@ class StubContext:
         """
         return self._state
 
-    def get_state(self, addresses, timeout=None):
+    def get_state(self, addresses):
         """
-        Get list of addresses with its data as key-value tuple.
+        Get a list of addresses with its data as key-value tuple from state.
 
         Return list of addresses:
             - if address match requested addresses;
@@ -62,7 +81,6 @@ class StubContext:
         response = []
 
         for address in addresses:
-
             if address in self._state and self._state.get(address) is not None:
                 response.append(
                     (address, self._state.get(address)),
@@ -70,15 +88,52 @@ class StubContext:
 
         return response
 
-    def set_state(self, entries, timeout=None):
-        pass
+    def set_state(self, entries):
+        """
+        Set a list of addresses with its data as key-value tuple to state.
 
-    def add_event(self, event_type, attributes, data):
-        pass
+        Arguments:
+            entries (list of (str, str) tuples or None): list of addresses-data as tuple.
+
+        Raises:
+            AuthorizationError: if some of entries address isn't presented in outputs.
+
+        Returns list of addresses that were set.
+        """
+        response = []
+
+        for address, data in entries:
+
+            if address not in self.outputs:
+                entries_addresses = [entry[0] for entry in entries]
+                raise AuthorizationException('Tried to set unauthorized address: {}'.format(entries_addresses))
+
+            self._state.update({
+                address: data,
+            })
+
+            response.append(address)
+
+        return response
+
+    def add_event(self, event_type, attributes=None, data=None):
+        """
+        Add a new event to the events list.
+
+        Arguments:
+            event_type (str): a type of the event.
+            attributes (list of (str, str) tuples or None): an event attributes.
+            data (bytes or None): Additional information about the event.
+        """
+        if attributes is None:
+            attributes = []
+
+        event = StubContextEvent(event_type=event_type, attributes=attributes, data=data)
+        self._events.append(event)
 
     def events(self):
         """
-        Get stub context events list.
+        Get a stub context events list.
 
         Returns list of the `StubContextEvent` objects.
         """
