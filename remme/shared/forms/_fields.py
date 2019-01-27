47714887988efe34b7f06cfa92d2f401eecf76f6
Python-script-writer <kontorchuk@gmail.com>
2019-01-28 00:12:51 +0200

    [REM-933] Fix invalid messege for method: get_atomic_swap_info, get_batch_status, get_public_key_info, get_public_key_list with *_id=0.

--- remme/shared/forms/_fields.py
+++ remme/shared/forms/_fields.py
@@ -4,7 +4,7 @@ from wtforms import fields, validators
 class AddressField(fields.StringField):
 
     validators = [
-        validators.DataRequired(message='Missed address'),
+        validators.InputRequired(message='Missed address'),
         validators.Regexp('[0-9a-f]{70}',
                           message='Address is not of a blockchain token type.')
     ]
@@ -13,7 +13,7 @@ class AddressField(fields.StringField):
 class SwapIDField(fields.StringField):
 
     validators = [
-        validators.DataRequired(message='Missed swap_id'),
+        validators.InputRequired(message='Missed swap_id'),
         validators.Regexp('[0-9a-f]{64}',
                           message='Incorrect atomic swap identifier.')
     ]
@@ -22,7 +22,7 @@ class SwapIDField(fields.StringField):
 class IDField(fields.StringField):
 
     validators = [
-        validators.DataRequired(message='Missed id'),
+        validators.InputRequired(message='Missed id'),
         validators.Regexp('[0-9a-f]{128}',
                           message='Incorrect identifier.')
     ]
