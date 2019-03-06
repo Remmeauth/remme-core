134e8fb2583d9d2448d23dc46320aa445f74f983
Anastasiia Bilova <anastasiabilova@gmail.com>
2019-03-06 15:11:38 +0200

    REM-1151: move node account to separate folder, add address validation, create tests

--- remme/rpc_api/account.py
+++ remme/rpc_api/account.py
@@ -12,21 +12,15 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ------------------------------------------------------------------------
-import json
 import logging
-
-from google.protobuf.json_format import MessageToJson
 
 from remme.shared.forms import get_address_form
 from remme.clients.account import AccountClient
-from remme.clients.basic import BasicClient
-from remme.protos.account_pb2 import NodeAccount
 from .utils import validate_params
 
 __all__ = (
     'get_balance',
     'get_public_keys_list',
-    'get_node_account',
 )
 
 
@@ -45,17 +39,3 @@ async def get_public_keys_list(request):
     client = AccountClient()
     address = request.params['public_key_address']
     return await client.get_pub_keys(address)
-
-
-async def get_node_account(request):
-
-    client = BasicClient()
-
-    node_address = request.params['node_account_address']
-    raw_account = await client.get_value(node_address)
-
-    node_account = NodeAccount()
-    node_account.ParseFromString(raw_account)
-
-    data = MessageToJson(message=node_account, preserving_proto_field_name=True, including_default_value_fields=True)
-    return json.loads(data)
--- testing/unit/rpc/test_node_account.py
+++ testing/unit/rpc/test_node_account.py
@@ -1,12 +1,15 @@
 import pytest
+from aiohttp_json_rpc.exceptions import RpcInvalidParamsError
 
-from remme.protos.account_pb2 import NodeAccount
-from remme.rpc_api.account import get_node_account
+from remme.protos.node_account_pb2 import NodeAccount
+from remme.rpc_api.node_account import get_node_account
 from testing.utils._async import return_async_value
+
+NODE_ACCOUNT_ADDRESS = 'node_account_address'
 
 
 @pytest.mark.asyncio
-async def test_get_node_account(mocker):
+async def test_get_node_account(mocker, request_):
     """
     Case: get node account data.
     Expect: dictionary with node account data.
@@ -19,20 +22,16 @@ async def test_get_node_account(mocker):
 
     serialize = node_account.SerializeToString()
 
-    class Request:
-
-        @property
-        def params(self):
-            return {
-                'node_account_address': 'eeecb926d7a378a639a68c096ffb0bc065a692c9a4d71dd032eed8c26227ca9602adc5',
-            }
-
-    request = Request()
-
     mock_get_block_info = mocker.patch('remme.clients.basic.BasicClient.get_value')
     mock_get_block_info.return_value = return_async_value(serialize)
 
-    result = await get_node_account(request)
+    node_account_address = 'eeecb926d7a378a639a68c096ffb0bc065a692c9a4d71dd032eed8c26227ca9602adc5'
+
+    request_.params = {
+        NODE_ACCOUNT_ADDRESS: node_account_address,
+    }
+
+    result = await get_node_account(request_)
 
     expected_result = {
         'balance': str(node_account.balance),
@@ -44,3 +43,36 @@ async def test_get_node_account(mocker):
     }
 
     assert expected_result == result
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize('invalid_address', ['12345', 12345, True])
+async def test_get_node_account_with_invalid_address(request_, invalid_address):
+    """
+    Case: get node account data with invalid address.
+    Expect: address is not of a blockchain token type error message.
+    """
+    request_.params = {
+        NODE_ACCOUNT_ADDRESS: invalid_address,
+    }
+
+    with pytest.raises(RpcInvalidParamsError) as error:
+        await get_node_account(request_)
+
+    assert 'Address is not of a blockchain token type.' == error.value.message
+
+
+@pytest.mark.asyncio
+async def test_get_node_account_without_address(request_):
+    """
+    Case: get node account data without address.
+    Expect: missed address error message.
+    """
+    request_.params = {
+        NODE_ACCOUNT_ADDRESS: None,
+    }
+
+    with pytest.raises(RpcInvalidParamsError) as error:
+        await get_node_account(request_)
+
+    assert 'Missed address.' == error.value.message
