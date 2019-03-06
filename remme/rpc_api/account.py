0db91c9e9b3288313356612ba6f2cd9367d7591e
Anastasiia Bilova <anastasiabilova@gmail.com>
2019-03-06 19:26:56 +0200

    REM-1138: refactor method get_balance, create tests for address verification function

--- remme/rpc_api/account.py
+++ remme/rpc_api/account.py
@@ -14,29 +14,27 @@
 # ------------------------------------------------------------------------
 import logging
 
-from remme.shared.forms import get_address_form
 from remme.clients.account import AccountClient
-
-from .utils import validate_params
+from remme.rpc_api.utils import validate_params
+from remme.shared.forms import get_address_form
 
 __all__ = (
     'get_balance',
     'get_public_keys_list',
 )
 
-
 logger = logging.getLogger(__name__)
+
+client = AccountClient()
 
 
 @validate_params(get_address_form('public_key_address'))
 async def get_balance(request):
-    client = AccountClient()
     address = request.params['public_key_address']
     return await client.get_balance(address)
 
 
 @validate_params(get_address_form('public_key_address'))
 async def get_public_keys_list(request):
-    client = AccountClient()
     address = request.params['public_key_address']
     return await client.get_pub_keys(address)
