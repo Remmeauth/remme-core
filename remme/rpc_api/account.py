4a31bc5b8618d196be7502cf1f580efebb30f20e
Dmytro Striletskyi <dmytro.striletskyi@gmail.com>
2019-03-26 17:53:19 +0200

    Separate excess parameters form logic to single class

--- remme/rpc_api/account.py
+++ remme/rpc_api/account.py
@@ -27,11 +27,65 @@ logger = logging.getLogger(__name__)
 
 client = AccountClient()
 
+from aiohttp_json_rpc import RpcInvalidParamsError
+from remme.shared.forms._fields import AddressField
+from werkzeug.datastructures import MultiDict
+from wtforms import Form
 
-@validate_params(get_address_form('public_key_address'))
+
+class ValidateExcessIncomingParameters(Form):
+
+    def __init__(self, formdata=None, obj=None, prefix='', data=None, meta=None, **kwargs):
+        self.excess_incoming_parameters = []
+        super().__init__(formdata, obj, prefix, data, meta, **kwargs)
+
+    def process(self, formdata=None, obj=None, data=None, **kwargs):
+        """
+        Process the form data.
+
+        The one of the goals of the form is to detect excess incoming parameters.
+        If these parameters are presented, return according error message.
+        """
+        super().process(formdata, obj, data, **kwargs)
+
+        passed_to_form_parameters_keys = formdata.to_dict().keys()
+        existing_form_fields_keys = dict(self._fields).keys()
+
+        for passed_key in passed_to_form_parameters_keys:
+            if passed_key not in existing_form_fields_keys:
+                self.excess_incoming_parameters.append(passed_key)
+
+    def validate(self):
+        """
+        Validate the form data.
+
+        If excess incoming parameters are presented, add excess incoming parameters error message with
+        the list of these parameters.
+        """
+        if self.excess_incoming_parameters:
+            self.errors['error'] = [
+                f'Wrong params keys: {self.excess_incoming_parameters}',
+            ]
+            return False
+
+        return super().validate()
+
+
+class GetAccountBalanceForm(ValidateExcessIncomingParameters):
+
+    public_key_address = AddressField()
+
+
 async def get_balance(request):
-    address = request.params['public_key_address']
-    return await client.get_balance(address)
+    """
+    Get account balance by account address from public key.
+    """
+    form = GetAccountBalanceForm(MultiDict(request.params))
+
+    if not form.validate():
+        raise RpcInvalidParamsError(message=form.errors)
+
+    return await client.get_balance(address=form.public_key_address.data)
 
 
 @validate_params(get_address_form('public_key_address'))
--- testing/unit/rpc/test_account.py
+++ testing/unit/rpc/test_account.py
@@ -17,13 +17,13 @@ async def test_get_balance(mocker, request_):
     Expect: list with addresses.
     """
     public_key_address = '112007081971dec92814033df35188ce17c740d5e58d7632c9528b61a88a4b4cde51e1'
-    expected_result = 0
+    expected_result = 1000
 
     mock_get_block_info = mocker.patch('remme.clients.account.AccountClient.get_balance')
     mock_get_block_info.return_value = return_async_value(expected_result)
 
     request_.params = {
-        PUBLIC_KEY_ADDRESS: public_key_address,
+        'public_key_address': public_key_address,
     }
 
     result = await get_balance(request_)
@@ -39,13 +39,15 @@ async def test_get_balance_with_invalid_address(request_, invalid_public_key_add
     Expect: address is not of a blockchain token type error message.
     """
     request_.params = {
-        PUBLIC_KEY_ADDRESS: invalid_public_key_address,
+        'public_key_address': invalid_public_key_address,
     }
 
     with pytest.raises(RpcInvalidParamsError) as error:
         await get_balance(request_)
 
-    assert 'Address is not of a blockchain token type.' == error.value.message
+    assert {
+        'public_key_address': ['Address is not of a blockchain token type.'],
+    } == error.value.message
 
 
 @pytest.mark.asyncio
@@ -56,13 +58,33 @@ async def test_get_balance_without_address(request_, address_is_none):
     Expect: missed address error message.
     """
     request_.params = {
-        PUBLIC_KEY_ADDRESS: address_is_none,
+        'public_key_address': address_is_none,
     }
 
     with pytest.raises(RpcInvalidParamsError) as error:
         await get_balance(request_)
 
-    assert 'Missed address.' == error.value.message
+    assert {
+        'public_key_address': ['Missed address.'],
+    } == error.value.message
+
+
+@pytest.mark.asyncio
+async def test_get_balance_excess_parameters(request_):
+    """
+    Case: get balance without address.
+    Expect: missed address error message.
+    """
+    request_.params = {
+        'some_excess_parameter': 567890,
+    }
+
+    with pytest.raises(RpcInvalidParamsError) as error:
+        await get_balance(request_)
+
+    assert {
+        'error': ["Wrong params keys: ['some_excess_parameter']"]
+    } == error.value.message
 
 
 @pytest.mark.asyncio
