7ae19f3839f1a41e9df8fd0aacf821f3043fff27
ArtemFrantsiian <artemfrantsiian@gmail.com>
2019-05-16 14:13:58 +0300

    Fix bugs

--- remme/clients/block_info.py
+++ remme/clients/block_info.py
@@ -15,13 +15,21 @@ BLOCK_INFO_NAMESPACE = NAMESPACE + '00'
 
 class BlockInfoClient(BasicClient):
 
-    async def get_block_info_states(self, block_num, limit):
+    async def get_block_info_states(self, start, limit):
         states = await self.list_state(BLOCK_INFO_NAMESPACE, start, limit)
         return states['data']
+
+    async def get_blocks_for_block_info(self, start, limit):
+        blocks = await self.list_blocks(
+            start=f'0x{start}',
+            limit=limit,
+            reverse='',
+        )
+        return blocks['data']
     
     def get_block_info(self, state):
         bi = BlockInfo()
-        bi.ParseFromString(state)
+        bi.ParseFromString(base64.b64decode(state))
         return bi
 
     async def get_blocks_info(self, start, limit):
@@ -33,35 +41,36 @@ class BlockInfoClient(BasicClient):
         except Exception:
             return []
 
-        start = self.create_block_address(block_config.oldest_block if not init_start else start - 1)
+        start = block_config.oldest_block if not init_start else start
 
-        limit = min(block_config.latest_block if not init_limit else limit - 1, 100)
+        limit = min(block_config.latest_block + 2 if not init_limit else limit, 100)
 
         end = min(start + limit, block_config.latest_block) + 1
-        
+
+        LOGGER.debug(f'start: {start}, limit: {limit}, end: {end}')
+
+        start_block = self.create_block_address(start)
         # get all BlockInfo from states with given start (address of BlockInfo) and limit (number of items)
-        states = await self.get_block_info_states(start, limit)
+        states = await self.get_block_info_states(start_block, limit)
         # get all Blocks from start to limit.
         # Parameter start should correspond to this pattern 0x[a-f0-9]{16}.
         # Start is address of BlockInfo which has namespace and in the ending hex from block_num. 
         # So we should:
         # 1. get last 16 value from start as we need for list_blocks start parameter
         # 2. parse to int
-        # 3. add 1 for start from next block (because we don't need first block
+        # 3. add 2 for start from next block (because we don't need first block)
         # 4. parse to hex and ignore "0x"
         # 5. concatenate with 16 zeros (because we need 16 symbols and we don't know what length of our hex)
         # 6. get 16 value from concatenating string.
-        start_for_list_blocks = f'0000000000000000{hex(int(start[:16], 16) + 1)[2:]}'[:16]
-        # We should grab next block after limit because it's need for parsing last block for limit.
-        limit_for_list_blocks = limit + 1
-        blocks = (await self.list_blocks(start=f'0x{start_for_list_blocks}', limit=limit_for_list_blocks, reverse=''))['data']
+        start_for_list_blocks = f'0000000000000000{hex(int(start_block[-16:], 16) + 1)[2:]}'[-16:]
+        blocks = await self.get_blocks_for_block_info(start_for_list_blocks, limit)
         
         blocks_info = []
         for index, value in enumerate(states):
             bi = self.get_block_info(value['data'])
             
             try:
-                nc = bi.block_num + 1
+                nc = bi.block_num
                 if nc == block_config.latest_block + 1:
                     # for last taking votes from memory
                     seal_votes = await self.get_seal_for_last_block()
@@ -120,7 +129,7 @@ class BlockInfoClient(BasicClient):
     @staticmethod
     def interpret_block_info(block_info, votes):
         return {
-            "block_number": block_info.block_num + 1,
+            "block_number": block_info.block_num,
             "timestamp": block_info.timestamp,
             "previous_header_signature": block_info.previous_block_id,
             "signer_public_key": block_info.signer_public_key,
